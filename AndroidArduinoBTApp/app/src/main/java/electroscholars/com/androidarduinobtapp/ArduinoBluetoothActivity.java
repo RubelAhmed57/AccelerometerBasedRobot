de16ebe51a3f23b853ab59ac0ae87bc3b96f6660
Manash Mandal <manashmndl@gmail.com>
2015-12-05 18:25:59 +0600

    Android Arduino Bluetooth project files

---
+++ AndroidArduinoBTApp/app/src/main/java/electroscholars/com/androidarduinobtapp/ArduinoBluetoothActivity.java
@@ -0,0 +1,14 @@
+package electroscholars.com.androidarduinobtapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class ArduinoBluetoothActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_arduino_bluetooth);
+    }
+}
